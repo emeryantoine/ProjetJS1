dc6a771174784056642e177d58edc9e5d578d356
emeryantoine <enderswype@gmail.com>
2021-05-28 10:10:27 +0200

    Bruce Banner avec du JS lolilol

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,8 @@
-function displayWindowSize(){
+function updateBannerPadding(){
     // Get width and height of the window excluding scrollbars
     var w = document.documentElement.clientWidth;
-    var h = document.documentElement.clientHeight;
     
     // Display result inside a div element
-    console.log(w)
     if(w >= 415){
         document.getElementById("banner").style.padding = "74px 0px 0px 0px";
         return;
@@ -21,9 +19,26 @@ function displayWindowSize(){
         return;
     }
 }
+
+function updateBannertext(){
+    var w = document.documentElement.clientWidth;
+
+    if(w < 553){
+        document.getElementById("bannerCatchPhrase").style.display = "none";
+        document.getElementById("bannerBrandName").style.bottom = "-10px";
+    } else{
+        document.getElementById("bannerCatchPhrase").style.display = "block";
+        document.getElementById("bannerBrandName").style.bottom = "20px";
+    }
+    return
+}
     
 // Attaching the event listener function to window's resize event
-window.addEventListener("resize", displayWindowSize);
+window.addEventListener("resize", () => {
+    updateBannerPadding();
+    updateBannertext();
+}, false);
 
 // Calling the function for the first time
-displayWindowSize();
+updateBannertext();
+updateBannerPadding();
