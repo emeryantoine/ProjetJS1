09da1fc3c04f4664cd9ce7805643a9fde7c71ecf
emeryantoine <enderswype@gmail.com>
2021-05-31 14:11:23 +0200

    cart done, add delete items list

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ function LignePanier(code, qte, prix) {
         this.qteArticle += qte;
     }
     this.getPrixLigne = function () {
-        let resultat = this.prixArticle * this.qteArticle || this.qteArticle == "";
+        let resultat = (this.prixArticle * this.qteArticle * 100) / 100;
         return resultat;
     }
     this.getCode = function () {
@@ -58,7 +58,9 @@ function ajouter(btnIci) {
     let longueurTab = parseInt(document.getElementById("nbreLignes").innerHTML);
     if (longueurTab > 0) {
         for (let i = longueurTab; i > 0; i--) {
-            monPanier.ajouterArticle((tableau.rows[i].cells[0].innerHTML), parseFloat(tableau.rows[i].cells[1].innerHTML), parseFloat(tableau.rows[i].cells[2].innerHTML));
+            let qteAfterTransform = tableau.rows[i].cells[1].innerHTML;
+            qteAfterTransform = String(qteAfterTransform).substr(44, 1);
+            monPanier.ajouterArticle((tableau.rows[i].cells[0].innerHTML), parseFloat(qteAfterTransform), parseFloat(tableau.rows[i].cells[2].textContent));
             tableau.deleteRow(i);
         }
     }
@@ -69,17 +71,21 @@ function ajouter(btnIci) {
         let colonne1 = ligneTableau.insertCell(0);
         colonne1.innerHTML += ligne.getCode();
         let colonne2 = ligneTableau.insertCell(1);
-        colonne2.innerHTML += ligne.qteArticle;
+        /*colonne2.innerHTML += ligne.qteArticle;*/
+        colonne2.innerHTML += "<input type=\"number\" min=\"1\" max=\"9\" value=\"" + ligne.qteArticle + "\" class=\"input-panier\" id=\"" + ligne.getCode() + "-inputId\"></input>"
         let colonne3 = ligneTableau.insertCell(2);
-        colonne3.innerHTML += ligne.prixArticle;
+        // colonne3.innerHTML += ligne.prixArticle;
+        colonne3.innerHTML += "<p id=\"" + ligne.getCode() + "-itemPrice\">" + ligne.prixArticle + "</p>"
         let colonne4 = ligneTableau.insertCell(3);
-        colonne4.innerHTML += ligne.getPrixLigne();
+        /*colonne4.innerHTML += ligne.getPrixLigne();*/
+        colonne4.innerHTML += "<p id=\"" + ligne.getCode() + "-sousTT\">" + ligne.getPrixLigne() + "</p>"
         let colonne5 = ligneTableau.insertCell(4);
         colonne5.innerHTML += "<button class=\"btn btn-dark\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Retirer</button>";
     }
     document.getElementById("prixTotal").innerHTML = monPanier.getPrixPanier() + " €";
     document.getElementById("nbreLignes").innerHTML = longueur;
 
+    manageEvent();
 }
 
 // Supprimer une ligne complête
@@ -90,7 +96,9 @@ function supprimer(code) {
     let longueurTab = parseInt(document.getElementById("nbreLignes").innerHTML);
     if (longueurTab > 0) {
         for (let i = longueurTab; i > 0; i--) {
-            monPanier.ajouterArticle(tableau.rows[i].cells[0].innerHTML, parseFloat(tableau.rows[i].cells[1].innerHTML), parseFloat(tableau.rows[i].cells[2].innerHTML));
+            let qteAfterTransform = tableau.rows[i].cells[1].innerHTML;
+            qteAfterTransform = String(qteAfterTransform).substr(44, 1);
+            monPanier.ajouterArticle((tableau.rows[i].cells[0].innerHTML), parseFloat(qteAfterTransform), parseFloat(tableau.rows[i].cells[2].textContent));
             tableau.deleteRow(i);
         }
     }
@@ -102,17 +110,44 @@ function supprimer(code) {
         let colonne1 = ligneTableau.insertCell(0);
         colonne1.innerHTML += ligne.getCode();
         let colonne2 = ligneTableau.insertCell(1);
-        colonne2.innerHTML += ligne.qteArticle;
+        /*colonne2.innerHTML += ligne.qteArticle;*/
+        colonne2.innerHTML += "<input type=\"number\" min=\"1\" max=\"9\" value=\"" + ligne.qteArticle + "\" class=\"input-panier\" id=\"" + ligne.getCode() + "-inputId\"></input>"
         let colonne3 = ligneTableau.insertCell(2);
-        colonne3.innerHTML += ligne.prixArticle;
+        // colonne3.innerHTML += ligne.prixArticle;
+        colonne3.innerHTML += "<p id=\"" + ligne.getCode() + "-itemPrice\">" + ligne.prixArticle + "</p>"
         let colonne4 = ligneTableau.insertCell(3);
-        colonne4.innerHTML += ligne.getPrixLigne();
+        /*colonne4.innerHTML += ligne.getPrixLigne();*/
+        colonne4.innerHTML += "<p id=\"" + ligne.getCode() + "-sousTT\">" + ligne.getPrixLigne() + "</p>"
         let colonne5 = ligneTableau.insertCell(4);
         colonne5.innerHTML += "<button class=\"btn btn-primary\" type=\"submit\" onclick=\"supprimer(this.parentNode.parentNode.cells[0].innerHTML)\"><span class=\"glyphicon glyphicon-remove\"></span> Retirer</button>";
     }
     document.getElementById("prixTotal").innerHTML = monPanier.getPrixPanier();
     document.getElementById("nbreLignes").innerHTML = longueur;
+
+    manageEvent();
 }
 
 // Modifier la quantité d'articles dans le panier
 
+function manageEvent(){
+    const events = document.querySelectorAll(".input-panier");
+    events.forEach(function (inputupdate) {
+        inputupdate.addEventListener('change', function(){
+            let qte = document.getElementById(this.id).value;
+            let price = document.getElementById(String(this.id).replace("-inputId", "-itemPrice")).textContent;
+
+            // document.getElementById(String(this.id).replace("-inputId", "-sousTT")).textContent = String(qte * price);
+            let target = "#" + String(this.id).replace("-inputId", "-sousTT")
+            document.querySelector(target).textContent = String((qte*price * 100 ) / 100);
+            
+
+            const ssTT = $("p[id*='sousTT']");
+            let total = 0.;
+            for (let i = 0; i < ssTT.length; i++) {
+                total += parseFloat(ssTT[i].textContent) * 100;
+            }
+            document.getElementById("prixTotal").textContent = total / 100;
+
+        })
+    })
+}
